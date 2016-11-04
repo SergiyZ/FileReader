2a9bcac46491402a433c911ff7035c119ce4ffff
Cristian Morataya <cmorataya@develsystems.com>
2016-11-04 10:28:49 -0600

    Encode the byte array to base64 string

--- src/com/ccmorataya/freader/Main.java
+++ src/com/ccmorataya/freader/Main.java
@@ -4,18 +4,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(System.getProperty("user.dir") + "\\1.pdf");
         Path path = Paths.get(System.getProperty("user.dir") + "/1.pdf");
+        byte[] bytesArray = new byte[0];
         try {
-            byte[] bytes = Files.readAllBytes(path);
-
-            System.out.println(bytes);
+            bytesArray = Files.readAllBytes(path);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        String decoded = Base64.getEncoder().encodeToString(bytesArray);
+        System.out.println(decoded);
     }
 }
